a2631a0c4d6427dc1c507b5dfdb02f62a3a18595
Mike Sander <mike.sander@kroger.com>
2022-10-04 12:46:50 -0400

    Update budget_1.go

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,8 +1,10 @@
 package module1
 
-// Budget stores budget information
-
-type Item Struct {
-	Max		float32
-	Items   []Item
+type Budget struct {
+	Max   float32
+	Items []Item
+}
+type Item struct {
+	Description string
+	Price       float32
 }
